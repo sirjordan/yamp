85164981d8f0ca7ba8718a0ceff8d6494252f9e7
Martin <sirjordan1988@gmail.com>
2017-01-22 19:54:48 +0200

    x
    x

--- dices.py
+++ dices.py
@@ -1,5 +1,7 @@
 import random
 
+MIN_DICE = 1
+MAX_DICE = 6
 DICE_COUNT = 5
 
 
@@ -8,15 +10,22 @@ def roll_dices():
     Roll and return 5 randomly generated dices
     :return: [] of 5 ints
     """
-    min_num = 1
-    max_num = 6
     dices = []
 
     for i in range(DICE_COUNT):
-        dice = random.randrange(min_num, max_num + 1)
+        dice = random.randrange(MIN_DICE, MAX_DICE + 1)
         dices.append(dice)
 
     return dices
+
+
+def count_equal(dices, num_to_match):
+    count = 0
+    for dice in dices:
+        if dice == num_to_match:
+            count += 1
+
+    return count
 
 
 def score_matching(dices, num_to_match):
@@ -24,12 +33,8 @@ def score_matching(dices, num_to_match):
     Any combination
     :return: The sum of dice with the number 'num_to_match'
     """
-    count = 0
-    for d in dices:
-        if d == num_to_match:
-            count += num_to_match
-
-    return count
+    count = count_equal(dices, num_to_match)
+    return count * num_to_match
 
 
 def score_n_of_a_kind(dices, n):
@@ -40,15 +45,57 @@ def score_n_of_a_kind(dices, n):
     if n != 3 and n != 4:
         raise ValueError('Parameter "n" must be 3 or 4 to count score')
 
-    for dice_num in range(DICE_COUNT):
-        n_of_count_num = 0
-        for d in dices:
-            if d == dice_num + 1:
-                n_of_count_num += 1
-        if n_of_count_num >= n:
+    for dice_num in range(MIN_DICE, MAX_DICE + 1):
+        n_of_kind = count_equal(dices, dice_num)
+        if n_of_kind >= n:
             return sum(dices)
 
     return 0
+
+
+def score_full(dices):
+    """
+    Full = Three of one number and two of another
+    :return: 25 if have Full
+    """
+    three_matched_num = 0
+    for dice_num in range(MIN_DICE, MAX_DICE + 1):
+        threes = count_equal(dices, dice_num) == 3
+        if threes:
+            three_matched_num = dice_num
+            break
+
+    two_matched_num = 0
+    for dice_num in range(MIN_DICE, MAX_DICE + 1):
+        if dice_num != three_matched_num:
+            twos = count_equal(dices, dice_num)
+            if twos == 2:
+                two_matched_num = twos
+                break
+
+    if three_matched_num > 0 and two_matched_num > 0:
+        return 25
+    else:
+        return 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
